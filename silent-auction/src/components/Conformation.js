306b9c76b5bdfb76330966ec1fdf5b51bb17198c
unknown <clmonitor2013@gmail.com>
2020-05-28 21:59:37 -0400

    Builidin confer page

--- silent-auction/src/components/Conformation.js
+++ silent-auction/src/components/Conformation.js
@@ -2,23 +2,27 @@ import '../css/index.css';
 
 import React, { useEffect } from 'react';
 import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Conformation = props => {
 
-    function handleSubmit() {
+    const history = useHistory();
 
+    function goHome() {
+        history.push("/");
     }
     
     useEffect(() => {
-       //console.log(props.response); // result: 'some_value'
+       console.log(props);
      }, [props.response]);
     
     return (
 
         <div className="confirm">
-            <h1>Thank you!</h1>
-            <div className="buttonContainer" onClick={handleSubmit}>
-            <div className="signup">Submit</div></div>
+            <h1>Thank you! {props.user}</h1>
+            
+            <div className="buttonContainer" onClick={goHome}><div className="home">Home</div></div>
+            
         </div>
 
     );
